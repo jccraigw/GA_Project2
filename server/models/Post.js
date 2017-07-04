77ba9bcf815892653f468e6121d5544a48e05b27
Julian Craigwell-Graham <julian@julians-mbp.attlocal.net>
2017-07-04 15:23:48 -0500

    user, comment and post models created

---
+++ server/models/Post.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+
+    link: String,
+    description: String,
+    comments: [{{type: mongoose.Schema.Types.ObjectId, ref: 'Comments'}}]
+
+});
+
+var postModel = mongoose.model('Post', PostSchema);
+
+module.exports = postModel;
---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+
+	name: String,
+    email: String,
+    password: String,
+    title: String,
+    location: String,
+    image: String,
+    friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+    posts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Post'}]
+
+});
+
+var userModel = mongoose.model('User', UserSchema);
+
+module.exports = userModel;
+
+
