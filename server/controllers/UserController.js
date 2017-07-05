8a784bde6b2a257a419a0b86c7614d7c15e9ae3e
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-07-05 12:45:31 -0500

    post request to join working

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -12,7 +12,44 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.get('/', function(req, res){
 
 	//display home page when user goes to localhost:3000/
-	res.render('home');
+	//display a list of users on page
+
+	User.find(function(err, users){
+
+		var allUsers = {users: users};
+
+		res.render('home', allUsers);
+	})
+	
+
+})
+
+//post request to /join that will add new users to the database
+//bcrypt password for security
+router.post('/join', function(req, res){
+
+	//print the request to the console
+	console.log(req.body);
+
+	bcrypt.hash(req.body.password, 10, function(error, hash){
+
+		//create new user and save to collection
+		var user = new User({
+
+			name: req.body.name,
+    		email: req.body.email,
+    		password: hash,
+    		title: req.body.title,
+    		location: req.body.title,
+    		image: req.body.image,
+    		
+		})
+
+		user.save();
+	})
+
+
+		res.send("success");
 })
 
 module.exports = router;
