18efa53ce2e35814fe14e6dedad4529be60c2542
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-07-15 17:52:31 -0500

    testing touch

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -72,27 +72,12 @@ $("nav a").on("click", function(e){
 // })
 
 
-var myElement = document.getElementById('image_post2');
+ 
+$('.image_post2').hammer().on("doubletap", function(){
 
-// We create a manager object, which is the same as Hammer(), but without the presetted recognizers. 
-var mc = new Hammer.Manager(myElement);
-
-
-// Tap recognizer with minimal 2 taps
-mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
-// Single tap recognizer
-mc.add( new Hammer.Tap({ event: 'singletap' }) );
-
-
-// we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
-mc.get('doubletap').recognizeWith('singletap');
-// we only want to trigger a tap, when we don't have detected a doubletap
-mc.get('singletap').requireFailure('doubletap');
-
-
-mc.on("doubletap", function(ev) {
+      
       $('.like').click();
-});
+  })
 
 
 		//profileimage upload process
