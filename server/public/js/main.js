e1c9dd49cbce1828204d3974c7c0c762fed719c8
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-07-15 17:47:17 -0500

    testing touch

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -2,6 +2,8 @@ console.log("main.js connected!")
 
 var imageUpload;
 
+
+
 $("document").ready(function() {
   $('.like').hide();
 	//hide profile image button uploader
@@ -64,11 +66,33 @@ $("nav a").on("click", function(e){
 })
 
 //on double click of image trigger like button click
-$(".image_post").dblclick(function(e){
-		e.preventDefault()
-		$('.like').click();
-})
+// $(".image_post").dblclick(function(e){
+// 		e.preventDefault()
+// 		$('.like').click();
+// })
 
+
+var myElement = document.getElementById('image_post2');
+
+// We create a manager object, which is the same as Hammer(), but without the presetted recognizers. 
+var mc = new Hammer.Manager(myElement);
+
+
+// Tap recognizer with minimal 2 taps
+mc.add( new Hammer.Tap({ event: 'doubletap', taps: 2 }) );
+// Single tap recognizer
+mc.add( new Hammer.Tap({ event: 'singletap' }) );
+
+
+// we want to recognize this simulatenous, so a quadrupletap will be detected even while a tap has been recognized.
+mc.get('doubletap').recognizeWith('singletap');
+// we only want to trigger a tap, when we don't have detected a doubletap
+mc.get('singletap').requireFailure('doubletap');
+
+
+mc.on("doubletap", function(ev) {
+      $('.like').click();
+});
 
 
 		//profileimage upload process
