8578d336e4f37b3eda1a4d4299c27c418a870782
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-07-18 20:57:07 -0500

    log out pop up working

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -7,7 +7,7 @@ var imageUpload;
 $("document").ready(function() {
   $('.like').hide();
 	//hide profile image button uploader
-//$('#profileImage').hide();
+$('#profileImage').hide();
 	//$('.descriptionEdit_toggle').hide();
 	$('#uploadPost').hide();
 	//$('.gear_background').hide();
@@ -48,8 +48,8 @@ $("document").ready(function() {
 	$("nav a").toggle();
 
 	//on click of gear icon click image button uploader
-	$('#gear').click(function(e){
-
+	$('.edit_profile_button').click(function(e){
+    e.preventDefault()
 		$('#profileImage').click();
 
 	});
@@ -357,6 +357,18 @@ jQuery(document).ready(function($) {
   $(document).ready(function() {
     $('.lab-slide-up').find('a').attr('data-toggle', 'modal');
     $('.lab-slide-up').find('a').attr('data-target', '#lab-slide-bottom-popup');
+  });
+
+});
+jQuery(document).ready(function($) {
+  // // auto timer
+  // setTimeout(function() {
+  //   $('#lab-slide-bottom-popup').modal('show');
+  // }, 5000); // optional - automatically opens in xxxx milliseconds
+
+  $(document).ready(function() {
+    $('.lab-slide-up').find('a').attr('data-toggle', 'modal');
+    $('.lab-slide-up').find('a').attr('data-target', '#lab-slide-bottom-popup2');
   });
 
 });
