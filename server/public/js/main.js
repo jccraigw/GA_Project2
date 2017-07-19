f2e83d942448e337bc3a0993c787ec3b044ebc7a
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-07-18 21:36:32 -0500

    text box grows with input

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -373,6 +373,12 @@ jQuery(document).ready(function($) {
 
 });
 
+$('textarea').on({input: function(){
+    var totalHeight = $(this).prop('scrollHeight') - parseInt($(this).css('padding-top')) - parseInt($(this).css('padding-bottom'));
+    $(this).css({'height':totalHeight});
+}
+});
+
 
 
 
